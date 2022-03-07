f670736e019d945e215d76bd2b10c2a8255e10c5
kantithat <kantithat.kanfung@gmail.com>
2022-03-07 16:13:08 +0700

    ทดสอบการแจ้งเตือนผ่าน ไลน์

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	date_default_timezone_set("Asia/Bangkok");
+
+	$sToken = "6PDLMlRaPpcBCfiA0GMejEWoZF6JvpoFg1CJUmZYpx0";
+	$sMessage = "มีรายการสั่งซื้อเข้าจ้า....";
+
+	
+	$chOne = curl_init(); 
+	curl_setopt( $chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify"); 
+	curl_setopt( $chOne, CURLOPT_SSL_VERIFYHOST, 0); 
+	curl_setopt( $chOne, CURLOPT_SSL_VERIFYPEER, 0); 
+	curl_setopt( $chOne, CURLOPT_POST, 1); 
+	curl_setopt( $chOne, CURLOPT_POSTFIELDS, "message=".$sMessage); 
+	$headers = array( 'Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer '.$sToken.'', );
+	curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers); 
+	curl_setopt( $chOne, CURLOPT_RETURNTRANSFER, 1); 
+	$result = curl_exec( $chOne ); 
+
+	//Result error 
+	if(curl_error($chOne)) 
+	{ 
+		echo 'error:' . curl_error($chOne); 
+	} 
+	else { 
+		$result_ = json_decode($result, true); 
+		echo "status : ".$result_['status']; echo "message : ". $result_['message'];
+	} 
+	curl_close( $chOne );   
+?>
+</body>
+</html>
